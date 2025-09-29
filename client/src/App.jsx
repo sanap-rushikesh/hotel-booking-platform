6993eca33348c96f4d8d4cdc9f5b83c8bbbd6e60
Rushikesh Sanap <sanaprushikesh18@gmail.com>
2025-09-29 09:52:47 +0530

    Create and style Navbar component with sticky scroll effect using React Router DOM

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import {useLocation } from 'react-router-dom'
 
 const App = () => {
+
+  const isOwnerPath = useLocation().pathname.includes("owner");
+
   return (
     <div>
-      
+      {!isOwnerPath && <Navbar />}
     </div>
   )
 }
