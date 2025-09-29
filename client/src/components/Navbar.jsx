d7384b8a6fc60246af1a3666b4af0a8ec42d8535
Rushikesh Sanap <sanaprushikesh18@gmail.com>
2025-09-29 22:43:52 +0530

    Create home page section

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
     }, []);
 
     return (
-            <nav className={`fixed top-0 left-0 bg-indigo-500 w-full flex items-center justify-between px-4 md:px-16 lg:px-24 xl:px-32 transition-all duration-500 z-50 ${isScrolled ? "bg-white/80 shadow-md text-gray-700 backdrop-blur-lg py-3 md:py-4" : "py-4 md:py-6"}`}>
+            <nav className={`fixed top-0 left-0 w-full flex items-center justify-between px-4 md:px-16 lg:px-24 xl:px-32 transition-all duration-500 z-50 ${isScrolled ? "bg-white/80 shadow-md text-gray-700 backdrop-blur-lg py-3 md:py-4" : "py-4 md:py-6"}`}>
 
                 {/* Logo */}
 
@@ -50,7 +50,7 @@ const Navbar = () => {
                 <div className="hidden md:flex items-center gap-4">
                     <img src={assets.searchIcon} alt="search" className={`$
                     {isScrolled && 'invert'} h-7 transition-all duration-500 `} />
-                    <button className={`px-8 py-2.5 rounded-full ml-4 transition-all duration-500 ${isScrolled ? "text-white bg-black" : "bg-white text-black"}`}>
+                    <button className={`px-8 py-2.5 rounded-full ml-4 transition-all duration-500 ${isScrolled ? "text-white bg-black" : "bg-black text-white"}`}>
                         Login
                     </button>
                 </div>
